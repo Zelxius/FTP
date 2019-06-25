ac884fd03f3ad235056edff62860234d7c4019c5
Zelxius <kiara_itzel@hotmail.com>
2019-06-24 23:59:23 -0500

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UploadBtn = new System.Windows.Forms.Button();
+            this.FtpBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UploadBtn
+            // 
+            this.UploadBtn.Location = new System.Drawing.Point(322, 198);
+            this.UploadBtn.Name = "UploadBtn";
+            this.UploadBtn.Size = new System.Drawing.Size(151, 49);
+            this.UploadBtn.TabIndex = 0;
+            this.UploadBtn.Text = "Upload File";
+            this.UploadBtn.UseVisualStyleBackColor = true;
+            this.UploadBtn.Click += new System.EventHandler(this.UploadBtn_Click);
+            // 
+            // FtpBtn
+            // 
+            this.FtpBtn.Location = new System.Drawing.Point(322, 69);
+            this.FtpBtn.Name = "FtpBtn";
+            this.FtpBtn.Size = new System.Drawing.Size(150, 53);
+            this.FtpBtn.TabIndex = 1;
+            this.FtpBtn.Text = "FTP";
+            this.FtpBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FtpBtn);
+            this.Controls.Add(this.UploadBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UploadBtn;
+        private System.Windows.Forms.Button FtpBtn;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string user = "test1pweb";
+        private string password = "computadora321";
+        private string ftpAddress = "ftp://files.000webhost.com/public_html";
+        private string filePath = "C:\\Users\\kiara\\Documents\\Escuela\\Programación - Estructura y base de datos\\a20140912.txt";
+        private FtpWebRequest request;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FtpConnection();
+        }
+
+        private void FtpConnection()
+        {
+            this.request = (FtpWebRequest)FtpWebRequest.Create(this.ftpAddress + "/" + Path.GetFileName(this.filePath));
+            this.request.Method = WebRequestMethods.Ftp.UploadFile;
+            this.request.Credentials = new NetworkCredential(this.user, this.password);
+            this.request.UsePassive = true;
+            this.request.UseBinary = true;
+            this.request.KeepAlive = false;
+        }
+
+        private byte[] LoadFile()
+        {
+            FileStream stream = File.OpenRead(this.filePath);
+            byte[] buffer = new byte[stream.Length];
+
+            stream.Read(buffer, 0, buffer.Length);
+            stream.Close();
+            return buffer;
+        }
+
+        private void UploadFile(byte[] buffer)
+        {
+            Stream reqStream = this.request.GetRequestStream();
+            reqStream.Write(buffer, 0, buffer.Length);
+            reqStream.Close();
+        }
+
+        private void UploadBtn_Click(object sender, EventArgs e)
+        {
+            byte[] fileBuffer = LoadFile();
+            UploadFile(fileBuffer);
+        }
+    }
+}
